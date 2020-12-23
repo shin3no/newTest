8a78f2528b98def0e4dd89f98948bbeb02d3cea5
shin3no <53810985+shin3no@users.noreply.github.com>
2020-12-23 19:53:46 +0900

    Update crawling.py
    multiprocessing

--- crawling.py
+++ crawling.py
@@ -2,150 +2,159 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
 from psutil import virtual_memory
 import pandas as pd
-import numpy as np
-import time
+import multiprocessing
 import datetime
+import time
 
-tstart_time = time.time()
-# url = "https://play.google.com/store/apps/details?id=com.ipapas.sajulite&hl=ko&showAllReviews=true"
-#url = "https://play.google.com/store/apps/details?id=com.thingsflow.hellobot&hl=ko&showAllReviews=true"
-url = "https://play.google.com/store/apps/details?id=handasoft.mobile.divination&hl=ko&showAllReviews=true"
-#url = "https://play.google.com/store/apps/details?id=com.yantech.orient.tojung&hl=ko&showAllReviews=true"
-#url = "https://play.google.com/store/apps/details?id=com.un7qi3.forceteller&hl=ko&showAllReviews=true"
-driverPath = "D:\\shin2no\\chromedriver.exe"
-driver = webdriver.Chrome(driverPath)
-driver.get(url)
 
-# 리뷰 최하위까지 스크롤 및 더보기 버튼 클릭
-wait = WebDriverWait(driver, 180)
-driver.find_element_by_xpath("//div[@class='ry3kXd Ulgu9']/div[@class='MocG8c UFSXYb LMgvRb KKjvXb']").click()
-time.sleep(0.5)
-driver.find_element_by_xpath("//div[@class='OA0qNb ncFHed']/div[@data-value='2']").click()
-time.sleep(0.5)
-new_height = 0
-last_height = driver.execute_script("return document.body.scrollHeight")
-print(">>> \t last_height : %d \t new_height : %d" % (new_height, last_height))
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+def convert_date(item_date):
+    item_date = item_date.replace("년 ", "-").replace("월 ", "-").replace("일", "")
+    item_date = datetime.datetime.strptime(item_date, "%Y-%m-%d").date()
+    return item_date
 
-while 1:
-    wait.until(ec.invisibility_of_element_located((By.XPATH, "//div[@jsname='lYU69']/div[@class='Fx1lse']")))
+
+def merge_review(item_L_review, item_review):
+    if item_review == '' and item_L_review != '':
+        return item_L_review
+    else:
+        return item_review
+
+
+if __name__ == '__main__':
+    tstart_time = time.time()
+    # url = "https://play.google.com/store/apps/details?id=com.ipapas.sajulite&hl=ko&showAllReviews=true"
+    # url = "https://play.google.com/store/apps/details?id=com.thingsflow.hellobot&hl=ko&showAllReviews=true"
+    url = "https://play.google.com/store/apps/details?id=handasoft.mobile.divination&hl=ko&showAllReviews=true"
+    # url = "https://play.google.com/store/apps/details?id=com.yantech.orient.tojung&hl=ko&showAllReviews=true"
+    # url = "https://play.google.com/store/apps/details?id=com.un7qi3.forceteller&hl=ko&showAllReviews=true"
+    driverPath = "D:\\shin2no\\chromedriver.exe"
+    driver = webdriver.Chrome(driverPath)
+    driver.get(url)
+
+    # 리뷰 최하위까지 스크롤 및 더보기 버튼 클릭
+    wait = WebDriverWait(driver, 180)
+    driver.find_element_by_xpath("//div[@class='ry3kXd Ulgu9']/div[@class='MocG8c UFSXYb LMgvRb KKjvXb']").click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath("//div[@class='OA0qNb ncFHed']/div[@data-value='2']").click()
+    time.sleep(0.5)
+    new_height = 0
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    print(">>> \t last_height : %d \t new_height : %d" % (new_height, last_height))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    memory_usage = virtual_memory()[2]
+    while 1:
+        wait.until(ec.invisibility_of_element_located((By.XPATH, "//div[@jsname='lYU69']/div[@class='Fx1lse']")))
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    if new_height == last_height:
-        if driver.find_elements_by_xpath("//span[@class='RveJvd snByac']"):
-            driver.execute_script("arguments[0].click();",
-                                  driver.find_element_by_xpath("//span[@class='RveJvd snByac']"))
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        else:
-            for i in range(3):
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        memory_usage = virtual_memory()[2]
+
+        if new_height == last_height:
+            if driver.find_elements_by_xpath("//span[@class='RveJvd snByac']"):
+                driver.execute_script("arguments[0].click();",
+                                      driver.find_element_by_xpath("//span[@class='RveJvd snByac']"))
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
-                new_height = driver.execute_script("return document.body.scrollHeight")
-                if new_height != last_height:
-                    break
-                print(">>>>>> \t %d th last height checking and wait 2secs..." % (i + 1))
-            if new_height == last_height and not driver.find_elements_by_xpath("//span[@class='RveJvd snByac']"):
-                print("******** finished <<page scrawling>> ********")
-                break
             else:
-                print(">>>>>> \t last_height : %d \t new_height : %d \t memory_usage : %.1f%%" % (
-                    last_height, new_height, memory_usage))
-                last_height = new_height
-    # elif new_height > 100000:
-    #    break
-    elif memory_usage > 89.9 or new_height > 3000000:  # 메모리 부족으로 인한 종료 방지
-        print(">>> \t The usage of system memory exceeds 90%, stopping crawling.")
-        break
-    else:
-        print(">>> \t last_height : %d \t new_height : %d \t memory_usage : %.1f%%" % (
+                for i in range(3):
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    time.sleep(2)
+                    new_height = driver.execute_script("return document.body.scrollHeight")
+                    if new_height != last_height:
+                        break
+                    print(">>>>>> \t %d th last height checking and wait 2secs..." % (i + 1))
+                if new_height == last_height and not driver.find_elements_by_xpath("//span[@class='RveJvd snByac']"):
+                    print("******** finished <<page scrawling>> ********")
+                    break
+            print(">>>>>> \t last_height : %d \t new_height : %d \t memory_usage : %.1f%%" % (
             last_height, new_height, memory_usage))
-        last_height = new_height
-
-# 장문 리뷰 전체 리뷰 버튼 일괄 클릭 동작
-spread_review = driver.find_elements_by_xpath("//button[@jsaction='click:TiglPc']")
-for i in range(len(spread_review)):
-    if spread_review[i].is_displayed():
-        driver.execute_script("arguments[0].click();", spread_review[i])  # 클릭 대상이 가려져 execute_script()를 사용해야함
-        time.sleep(0.5)
-        print(">>> \t (%d/%d)th review button is clicked..." % ((i + 1), len(spread_review)))
-print("******** finished <<all review button>> clicking ********")
-
-
-# 크롤링
-
-start_time = time.time()
-t_cnt = len(driver.find_elements_by_xpath("//span[@class='IEFhEe']"))
-np_T_reviews = np.empty(t_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//span[@class='IEFhEe']")):
-    np_T_reviews[i] = element.text
-print("******** finished <<T_review >> crawling ********")
-
-r_cnt = len(driver.find_elements_by_xpath("//span[@jsname='bN97Pc']"))
-np_L_reviews = np.empty(r_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//span[@jsname='fbQN7e']")):
-    np_L_reviews[i] = element.text
-print("******** finished <<L_review : %s, %s>> crawling ********" % (r_cnt, t_cnt))
-
-ii = r_cnt - t_cnt
-np_reviews = np.empty(r_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//span[@jsname='bN97Pc']")):
-    if i >= ii:
-        if element.text == '' and np_L_reviews[i] != '':
-            np_reviews[i] = np_L_reviews[i]
-        elif element.text == '' and np_L_reviews[i] == '':
-            np_reviews[i] = np_T_reviews[i-ii]
+            last_height = new_height
+        elif new_height > 130000:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            break
+        elif memory_usage > 86.9 or new_height > 3000000:  # 크롬 메모리 부족으로 인한 종료 방지
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            print(">>> \t The usage of system memory exceeds 90%, stopping crawling.")
+            break
         else:
-            np_reviews[i] = element.text
-    else:
-        if element.text == '' and np_L_reviews[i] != '':
-            np_reviews[i] = np_L_reviews[i]
-        else:
-            np_reviews[i] = element.text
-print("******** finished <<review>> crawling ********")
-print(str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            print(">>> \t last_height : %d \t new_height : %d \t memory_usage : %.1f%%" % (
+                last_height, new_height, memory_usage))
+            last_height = new_height
+    time.sleep(5)
 
-start_time = time.time()
-np_dates = np.empty(r_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//div[@class='bAhLNe kx8XBd']/div/span[@class='p2TkOb']")):
-    tmp = element.text.replace("년 ", "-").replace("월 ", "-").replace("일", "")
-    np_dates[i] = datetime.datetime.strptime(tmp, "%Y-%m-%d").date()
-print("******** finished <<date>> crawling and replace ********")
-print(str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+    # 장문 리뷰 전체 리뷰 버튼 일괄 클릭 동작
+    spread_review = driver.find_elements_by_xpath("//button[@jsaction='click:TiglPc']")
+    for i in range(len(spread_review)):
+        if spread_review[i].is_displayed():
+            driver.execute_script("arguments[0].click();", spread_review[i])  # 클릭 대상이 가려져 execute_script()를 사용해야함
+            time.sleep(0.5)
+            print(">>> \t (%d/%d)th review button is clicked..." % ((i + 1), len(spread_review)))
+    print("******** finished << all_review button >> clicking ********")
+    time.sleep(5)
 
-np_likes = np.empty(r_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//div[@aria-label='이 리뷰가 유용하다는 평가를 받은 횟수입니다.']")):
-    np_likes[i] = element.text
-print("******** finished <<like>> crawling ********")
+    # 크롤링
+    num_cores = multiprocessing.cpu_count()
 
-start_time = time.time()
-np_stars = np.empty(r_cnt, dtype='object')
-for i, element in enumerate(driver.find_elements_by_xpath("//span[@class='nt2C1d']/div[@class='pf5lIe']/div[@role='img']")):
-    np_stars[i] = element.get_attribute('aria-label')[10:11]
-print("******** finished <<star>> crawling ********")
-print(str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+    start_time = time.time()
+    np_L_reviews = [element.text for i, element in enumerate(driver.find_elements_by_xpath("//span[@jsname='fbQN7e']"))]
+    print("******** finished << L_review : %s >> crawling ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
 
-# 데이터 통합
-start_time = time.time()
-np_results = np.empty((r_cnt, 4), dtype='object')
-for i in range(r_cnt):
-    np_results[i][0] = np_dates[i]
-    np_results[i][1] = np_stars[i]
-    np_results[i][2] = np_likes[i]
-    np_results[i][3] = np_reviews[i]
-print("******** finished <<review merge>> ********")
-print(str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+    start_time = time.time()
+    np_reviews = [element.text for i, element in enumerate(driver.find_elements_by_xpath("//span[@jsname='bN97Pc']"))]
+    for i, element in enumerate(np_reviews):
+        try:
+            checker = driver.find_element_by_xpath("//div[@jscontroller='H6eOGe'][%s]/div/div[2]/div[2]/span[@class='IEFhEe']" % (i + 1))
+            if checker:
+                print(i)
+                np_reviews[i] = checker.text + " " + element
+                print(np_reviews[i])
+        except NoSuchElementException:
+            continue
+    pool = multiprocessing.Pool(num_cores)
+    np_reviews = pool.starmap(merge_review, zip(np_L_reviews, np_reviews))
+    pool.close()
+    pool.join()
+    print("******** finished << review : %s >> crawling ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
 
-# csv 저장
-data = pd.DataFrame(np_results, dtype='object')
-data.columns = ['날짜', '평점', '동의', '리뷰']
-pkg_name = url.split('?id=')[-1].split('&')[0]
-date_format = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d_%H-%M-%S")
-file_name = pkg_name + "_" + date_format
-data.to_csv('D:\\google_review\\{}.csv'.format(file_name), encoding='utf-8-sig')
-print(str(datetime.timedelta(seconds=time.time() - tstart_time)).split(".")[0])
-driver.quit()
+    start_time = time.time()
+    np_dates = [element.text for i, element in
+                enumerate(driver.find_elements_by_xpath("//div[@class='bAhLNe kx8XBd']/div/span[@class='p2TkOb']"))]
+    pool = multiprocessing.Pool(num_cores)
+    np_dates = pool.map(convert_date, np_dates)
+    pool.close()
+    pool.join()
+    print("******** finished << date : %s >> crawling and replace ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+
+    start_time = time.time()
+    np_likes = [element.text for i, element in
+                enumerate(driver.find_elements_by_xpath("//div[@aria-label='이 리뷰가 유용하다는 평가를 받은 횟수입니다.']"))]
+    print("******** finished << like : %s >> crawling ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+
+    start_time = time.time()
+    np_stars = [element.get_attribute('aria-label')[10:11] for i, element in enumerate(
+        driver.find_elements_by_xpath("//span[@class='nt2C1d']/div[@class='pf5lIe']/div[@role='img']"))]
+    print("******** finished << star : %s >> crawling ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+
+    # 데이터 통합
+    start_time = time.time()
+    np_results = list(zip(np_dates, np_stars, np_likes, np_reviews))
+    print("******** finished << review merge : %s >> ********" %
+          str(datetime.timedelta(seconds=time.time() - start_time)).split(".")[0])
+
+    # csv 저장
+    data = pd.DataFrame(np_results, dtype='object')
+    data.columns = ['날짜', '평점', '동의', '리뷰']
+    pkg_name = url.split('?id=')[-1].split('&')[0]
+    date_format = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d_%H-%M-%S")
+    file_name = pkg_name + "_" + date_format
+    data.to_csv('D:\\google_review\\{}.csv'.format(file_name), encoding='utf-8-sig')
+    print(str(datetime.timedelta(seconds=time.time() - tstart_time)).split(".")[0])
+    driver.quit()
